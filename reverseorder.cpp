dd7fad0ffe4b3d90f5c45c97c60b3380a908cdf4
Vaishnavi-Zope <122734855+Vaishnavi-Zope@users.noreply.github.com>
2024-09-09 10:55:13 +0530

    Create reverseorder.cpp

---
+++ reverseorder.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[7];
+    cout<<"Enter 7 integers : ";
+    for(int i = 0;i<7;i++)
+    cin>>arr[i];
+     for(int j = 7;j>0;j--)
+     {
+      cout<<arr[j]<<" ";
+     }
+}
